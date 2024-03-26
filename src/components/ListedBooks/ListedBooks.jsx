cb53eecc967d92975e7fda7db0e1f3c8302389e7
dishamallik <dishamallik758@gmail.com>
2024-03-26 12:57:05 +0600

    created simple website

--- src/components/ListedBooks/ListedBooks.jsx
+++ src/components/ListedBooks/ListedBooks.jsx
@@ -1,10 +1,41 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 
 
 const ListedBooks = () => {
+    const [tabIndex, setTabIndex] = useState(0)
     return (
         <div>
-            <h1>listed Books</h1>
+            <div className="border-2 bg-slate-300  text-center mt-10 mb-10">Books</div>
+
+           <Tabs>
+            
+    
+        <Link 
+        // to=''
+         onClick={() =>setTabIndex(0)} className={`${tabIndex === 0 ? 'border border-b-4' : 'border-b' }`}>
+      <Tab>Read</Tab>
+      </Link>
+      <Link 
+    //   to={`wishlist`}
+       onClick={() =>setTabIndex(1)} className={`${tabIndex === 1 ? 'border border-b-4' : 'border-b' }`}>
+      <Tab>Wishlist</Tab>
+      </Link>
+    
+
+    <TabPanel>
+      <h2>Any content 1</h2>
+    </TabPanel>
+    <TabPanel>
+      <h2>Any content 2</h2>
+    </TabPanel>
+    
+  </Tabs>
         </div>
+
+        
     );
 };
 
