fc4033b297f73e97b5c448cbeaa92122d415395f
dishamallik <dishamallik758@gmail.com>
2024-03-28 13:33:40 +0600

    Created simple website

--- src/components/Read/Read.jsx
+++ src/components/Read/Read.jsx
@@ -65,7 +65,7 @@ const handleJobsFilter = filter =>{
                     displayJobs.map(job => <li key={job.bookId}>
                 
                 
-                <div className="  container mx-auto mt-10 card card-side bg-base-100 shadow-xl">
+                <div className="container mx-auto mt-10 card card-side bg-base-100 shadow-xl">
   <div className="lg:flex">
   <div>
   <img className="w-64 " src={job.image} alt="Movie"/>
@@ -73,25 +73,26 @@ const handleJobsFilter = filter =>{
   <div className="card-body">
     <h1 className="font-bold text-2xl">{job.bookName}</h1>
     <h1>By: {job.author} </h1>
-    <div className="flex gap-5">
+    <div className="lg:flex gap-5">
         <div className="font-bold">tag</div>
         <div><button className="border-2 rounded-lg bg-green-100 text-green-500">#{job.tags[0]}</button></div>
         <div><button className="border-2 rounded-lg bg-green-100 text-green-500">#{job.tags[1]}</button></div>
         <div> Year of Publishing: {job.yearOfPublishing}</div>
     </div>
 
-    <div className="flex gap-10">
+    <div className="lg:flex gap-10">
         <div> publisher:   { job.publisher} </div>
         <div> page: {job.totalPages}</div>
     </div>
 
     <div className="w-full border-t border-gray-300 border-line my-4"></div> 
 
-    <div className="flex gap-10">
+    <div className=" flex gap-10 ">
         <div><button className="border-2  w-auto h-10 rounded-lg bg-blue-100 text-blue-500">category:{job.category}</button></div>
         <div><button className="border-2  w-auto h-10 rounded-lg bg-red-100 text-red-500">rating:{job.rating}</button></div>
-        <Link to={`/job/${job.bookId}`}><button className="bg-green-500 btn">View Details</button></Link>
+        
     </div>
+    <Link to={`/job/${job.bookId}`}><button className="bg-green-500 border-2  w-auto h-10 rounded-lg btn">View Details</button></Link>
     
   </div>
   </div>
--- src/components/Wishlist/Wishlist.jsx
+++ src/components/Wishlist/Wishlist.jsx
@@ -41,25 +41,26 @@ const Wishlist = () => {
   <div className="card-body">
     <h1 className="font-bold text-2xl">{job.bookName}</h1>
     <h1>By: {job.author} </h1>
-    <div className="flex gap-5">
+    <div className="lg:flex gap-5">
         <div className="font-bold">tag</div>
         <div><button className="border-2 rounded-lg bg-green-100 text-green-500">#{job.tags[0]}</button></div>
         <div><button className="border-2 rounded-lg bg-green-100 text-green-500">#{job.tags[1]}</button></div>
         <div> Year of Publishing: {job.yearOfPublishing}</div>
     </div>
 
-    <div className="flex gap-10">
+    <div className="lg:flex gap-10">
         <div> publisher:   { job.publisher} </div>
         <div> page: {job.totalPages}</div>
     </div>
 
     <div className="w-full border-t border-gray-300 border-line my-4"></div> 
 
-    <div className="flex gap-10">
-        <div><button className="border-2  w-auto h-10 rounded-lg bg-blue-100 text-blue-500">category:{job.category}</button></div>
-        <div><button className="border-2  w-auto h-10 rounded-lg bg-red-100 text-red-500">rating:{job.rating}</button></div>
-        <Link to={`/job/${job.bookId}`}><button className="bg-green-500 btn">View Details</button></Link>
+    <div className="  lg:flex gap-10">
+        <div><button className="border-2  w-auto h-10 rounded-lg bg-blue-100 text-blue-500 mt-2">category:{job.category}</button></div>
+        <div><button className="border-2  w-auto h-10 rounded-lg bg-red-100 text-red-500 mt-2">rating:{job.rating}</button></div>
+        <Link to={`/job/${job.bookId}`}><button className="bg-green-500 btn border-2  w-auto h-10 rounded-lg mt-2">View Details</button></Link>
     </div>
+    
     
   </div>
   </div>
